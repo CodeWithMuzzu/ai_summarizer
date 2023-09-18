eef66a6e1ddf4b6b0c652b4894a145aca460d11d
codewithmuzzu <codewithmuzzu@gmail.com>
2023-09-18 15:18:17 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+  /** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        satoshi: ["Satoshi", "sans-serif"],
+        inter: ["Inter", "sans-serif"],
+      },
+      boxShadow: {
+        custom: "inset 10px -50px 94px 0 rgba(199, 199, 199, 0.2)",
+  
+      },
+    },
+  },
+  plugins: [],
+};
+
